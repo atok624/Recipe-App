b8294af1992853cd811483f01d19ec1441bd93f0
mutai-gilbert <mutaigilbert85@gmail.com>
2023-06-30 21:20:37 +0300

    Fix linter errors

--- spec/integration/recipe_public_spec.rb
+++ spec/integration/recipe_public_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'User index page', type: :system do
-    describe '  Recipe' do
-        before do
-            @user = User.create!(name: 'Test', email: 'test@localhost', password: 'password')
-            @recipe = Recipe.create!(user: @user, name: 'Test recipe', preparation_time: 1, cooking_time: 2, description: 'Test description', public: true)
-            @recipe = Recipe.create!(user: @user, name: 'Test recipe2', preparation_time: 1, cooking_time: 10, description: 'Test description2', public: false)
-            visit root_path
-        end
-        scenario 'Public recipe is displayed on public_recipes page' do
-            expect(page).to have_content 'Test recipe'
-        end
-
-        scenario 'Public recipe is not displayed on public_recipes page' do
-            expect(page).to_not have_content 'Test recipe2'
-        end
+  describe '  Recipe' do
+    before do
+      @user = User.create!(name: 'Test', email: 'test@localhost', password: 'password')
+      @recipe = Recipe.create!(user: @user, name: 'Test recipe', preparation_time: 1, cooking_time: 2,
+                               description: 'Test description', public: true)
+      @recipe = Recipe.create!(user: @user, name: 'Test recipe2', preparation_time: 1, cooking_time: 10,
+                               description: 'Test description2', public: false)
+      visit root_path
     end
+    scenario 'Public recipe is displayed on public_recipes page' do
+      expect(page).to have_content 'Test recipe'
+    end
+
+    scenario 'Public recipe is not displayed on public_recipes page' do
+      expect(page).to_not have_content 'Test recipe2'
+    end
+  end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 RSpec.describe Recipe, type: :model do
-    before :each do
-        @user = User.create!(name: 'user1', email: 'user1@localhost', password: 'password')
-        puts @user.errors.full_messages
-        @recipe = Recipe.create!(user: @user, name: 'Test recipe', preparation_time: 1, cooking_time: 2, description: 'Test description')
-
-    end
-    it ' Recipe model should be equal to the test recipe' do
-        expect(@recipe.name).to eq('Test recipe')
-        expect(@recipe.preparation_time).to eq(1)
-        expect(@recipe.cooking_time).to eq(2)
-        expect(@recipe.description).to eq('Test description')
-    end
-
+  before :each do
+    @user = User.create!(name: 'user1', email: 'user1@localhost', password: 'password')
+    puts @user.errors.full_messages
+    @recipe = Recipe.create!(user: @user, name: 'Test recipe', preparation_time: 1, cooking_time: 2,
+                             description: 'Test description')
+  end
+  it ' Recipe model should be equal to the test recipe' do
+    expect(@recipe.name).to eq('Test recipe')
+    expect(@recipe.preparation_time).to eq(1)
+    expect(@recipe.cooking_time).to eq(2)
+    expect(@recipe.description).to eq('Test description')
+  end
 end
