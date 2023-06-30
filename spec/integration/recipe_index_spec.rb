bbf1e962a4ecb13b51e7a27046dc7bb13332f3e0
mutai-gilbert <mutaigilbert85@gmail.com>
2023-06-30 21:04:40 +0300

    Add food_spec.rb file

--- spec/integration/recipe_index_spec.rb
+++ spec/integration/recipe_index_spec.rb
@@ -1,36 +1,33 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe 'Recipe', type: :system do
-    describe 'recipe index page' do
-        before do
-            @user = User.create!(name: 'Test', email: 'test@localhost', password: 'password')
-            @recipe = Recipe.create!(user: @user, name: 'Test recipe', preparation_time: 1, cooking_time: 2, description: 'Test description', public: true)
-            Recipe.create!(user: @user, name: 'Test recipe2', preparation_time: 1, cooking_time: 10, description: 'Test description2', public: true)
-            visit recipes_path
-
-            fill_in 'Email', with: 'test@localhost'
-            fill_in 'Password', with: 'password'
-            click_button 'Log in'
-        end
-
-        it 'Recipes List' do
-            expect(page).to have_content 'Test recipe'
-            expect(page).to have_content 'Test recipe2'
-        end
-        it 'Recipe description' do
-            expect(page).to have_content 'Test description'
-            expect(page).to have_content 'Test description2'
-        end
-
-        it 'When I click on that page, I expect to be taken to that page' do
-            click_link 'Test recipe'
-            expect(page).to have_current_path recipe_path(@recipe)
-        end
-        it 'When I click on a recipe remove btn, recipe is deleted.' do
-            click_button 'REMOVE', match: :first
-            expect(page).to_not have_content('Test description')
-            expect(page).to have_content('Test description2')
-        end
+  describe 'recipe index page' do
+    before do
+      @user = User.create(name: 'Test', email: 'test2@example.com', password: 'password')
+      @recipe = Recipe.create(user: @user, name: 'test recipe1', preparation_time: 1, cooking_time: 10,
+                              description: 'test test1')
+      Recipe.create(user: @user, name: 'test recipe2', preparation_time: 1, cooking_time: 10, description: 'test test2')
+      visit recipes_path
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      click_button 'Log in'
+    end
+    it 'Recipes list' do
+      expect(page).to have_content('test recipe1')
+      expect(page).to have_content('test recipe2')
+    end
+    it 'Recipes description' do
+      expect(page).to have_content('test test1')
+      expect(page).to have_content('test test2')
+    end
+    it 'When I click on a recipe, I am redirected to that recipe show page.' do
+      click_link 'test recipe1'
+      expect(page).to have_current_path(recipe_path(@recipe))
+    end
+    it 'When I click on a recipe remove btn, recipe is deleted.' do
+      click_button 'REMOVE', id: "remove-button-#{@recipe.id}"
+      expect(page).to_not have_content('test test1')
+      expect(page).to have_content('test test2')
+    end
+  end
 end
-end
-
---
+++ spec/models/food_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Food, type: :model do
+    before :each do
+        @user = User.create!(name: 'user1', email: 'user1@localhost', password: 'password')
+        puts @user.errors.full_messages
+        @test_food = Food.create!(user: @user, name: 'Test food', measurement_unit: 'kg', price: 1, quantity: 1)
+    end
+
+    it ' Food model should be equal to the test food' do
+        expect(@test_food.name).to eq('Test food')
+        expect(@test_food.measurement_unit).to eq('kg')
+        expect(@test_food.price).to eq(1)
+        expect(@test_food.quantity).to eq(1)
+    end
+end
